2ca5f5a180e1a22f97e0ab23ed557f9c25f734f4
Erika <erica.mellao2005@gmail.com>
2025-11-02 20:35:14 -0300

    Backend corriendo, agregué la entidad user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import { loginUser } from "../services/auth.service.js";
 import { createUser } from "../services/user.service.js";
 import { handleSuccess, handleErrorClient, handleErrorServer } from "../Handlers/responseHandlers.js";
-import { userValidation } from "../validations/user.validation.js";
 import { AppDataSource } from "../config/configDb.js";
 
 export async function login(req, res) {
@@ -51,4 +50,4 @@ export async function register(req, res) {
             handleErrorServer(res, 500, "Error interno del servidor", error.message);
         }
     }
-}
+} 
