c986792294f0febd5ef457db19fa7c10a983bf5a
saaieen <sayenbarra@gmail.com>
2025-12-22 22:21:05 -0300

    Modificación de un conflicto

--- backend/src/entities/BicycleEntity.js
+++ backend/src/entities/BicycleEntity.js
@@ -3,55 +3,56 @@
 import { EntitySchema } from 'typeorm';
 
 export const BicycleEntity = new EntitySchema({
-  name: 'Bicycle',
-  tableName: 'bicycles',
-  columns: {
-    id: {
-      type: Number,
-      primary: true,
-      generated: true,
-    },
-    brand: {
+    name: 'Bicycle',
+    tableName: 'bicycles',
+    columns: {
+        id: {
+        type: Number,
+        primary: true,
+        generated: true,
+        },
+        brand: {
         type: String,
-      nullable: false,
-    },
-    model: {
+        nullable: false,
+        },
+        model: {
         type: String,
-      nullable: false,
-    },
-    color: {
+        nullable: false,
+        },
+        color: {
         type: String,
-      nullable: false,
-    },
-    serialNumber: {
-      // opcional si se usa para reserva o control
+        nullable: false,
+        },
+        serialNumber: {
+        // opcional si se usa para reserva o control
         type: String,
-      nullable: true,
-      unique: true,
+        nullable: true,
+        unique: true,
+        },
+        photo: {
+        type: 'varchar',
+        nullable: true,
+        },
     },
-    photo: {
-      type: 'varchar',
-      nullable: true,
-    },
-  },
-  relations: {
-    user: {
-      target: 'User',
-      type: 'many-to-one',
-      joinColumn: true,
-      nullable: false,
-      onDelete: 'CASCADE',
-    },
-    reservations: {
-      target: 'Reservation',
-      type: 'one-to-many',
-      inverseSide: 'bicycle',
-    },
-    spaceLogs: {
-      target: 'SpaceLog',
-      type: 'one-to-many',
-      inverseSide: 'bicycle',
-    },
+    relations: {
+        user: {
+        target: 'User',
+        type: 'many-to-one',
+        joinColumn: true,
+        nullable: false,
+        onDelete: 'CASCADE',
+        },
+        reservations: {
+        target: 'Reservation',
+        type: 'one-to-many',
+        inverseSide: 'bicycle',
+        },
+        spaceLogs: {
+        target: 'SpaceLog',
+        type: 'one-to-many',
+        inverseSide: 'bicycle',
+        },
+
         bikerack: {
             type: 'many-to-one',
             target: 'Bikerack',
@@ -62,7 +63,7 @@ export const BicycleEntity = new EntitySchema({
         },
 
 
-  },
+    },
 });
 
 export default BicycleEntity;
