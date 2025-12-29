74f0fa9e860c08b0a0d174a0c3168967e3d83cd2
saaieen <sayenbarra@gmail.com>
2025-12-29 10:56:12 -0300

    Ultimos errores

--- frontend/src/pages/admin/BicicletasAdmin.jsx
+++ frontend/src/pages/admin/BicicletasAdmin.jsx
@@ -14,7 +14,7 @@ function BicicletasAdmin() {
     const navigate = useNavigate();
     
     // URL base
-    const API_URL = 'http://localhost:3000/api';
+    const API_URL = import.meta.env.VITE_API_URL;
     
     // Estados
     const [loading, setLoading] = useState(true);
--- frontend/src/services/history.service.js
+++ frontend/src/services/history.service.js
@@ -3,10 +3,10 @@ import apiService from './api.service.js';
 import { getToken } from './auth.service';
 
 // AÑADE ESTA LÍNEA (sin process.env):
-const API_URL = 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const HistoryService = {
-  // Corresponde a http://localhost:3000/api/history/occupancy
+
   getOccupancyHistory: async (page = 1, limit = 10) => {
     const token = getToken();
     const endpoint = `history/occupancy?page=${page}&limit=${limit}`;
@@ -18,7 +18,6 @@ const HistoryService = {
     return await response.json();
   },
 
-  // Corresponde a http://localhost:3000/api/history/guards
   getGuardsHistory: async (page = 1, limit = 10) => {
     const token = getToken();
     const endpoint = `history/guards?page=${page}&limit=${limit}`;
